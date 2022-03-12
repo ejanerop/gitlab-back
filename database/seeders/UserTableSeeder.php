a53d4fab52748b6391c3bff421aadcf6fe1a2782
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:02:07 -0500

    Updated seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
+
+use function PHPUnit\Framework\throwException;
 
 class UserTableSeeder extends Seeder
 {
@@ -15,11 +19,21 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = new User();
-        $user->name = 'Eric';
-        $user->email = 'ejanerop97@gmail.com';
-        $user->gitlab_token = '8iscuLB6M_PzSAjfEwY7';
-        $user->password = Hash::make('12345678');
-        $user->save();
+        if((env('DEFAULT_USER') !== null && env('DEFAULT_USER') !== '')
+        && (env('DEFAULT_USER_PASSWORD') !== null && env('DEFAULT_USER_PASSWORD') !== '')
+        && (env('DEFAULT_USER_GITLAB_TOKEN') !== null && env('DEFAULT_USER_GITLAB_TOKEN') !== '')
+        && (env('DEFAULT_USER_EMAIL') !== null && env('DEFAULT_USER_EMAIL') !== ''))
+        {
+            $user = new User();
+            $user->name = env('DEFAULT_USER');
+            $user->email = env('DEFAULT_USER_EMAIL');
+            $user->gitlab_token = env('DEFAULT_USER_GITLAB_TOKEN');
+            $user->password = Hash::make(env('DEFAULT_USER_PASSWORD'));
+            $user->save();
+        } else {
+            $this->command->error('Configure environment variables.');
+        }
+
+
     }
 }
